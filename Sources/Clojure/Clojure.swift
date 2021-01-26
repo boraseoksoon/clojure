5564d822396d9ca25e4d6d13fd2579a742b373da
seoksoon <boraseoksoon@gmail.com>
2021-01-26 18:45:45 -0500

    # refresh commit

--- Sources/Clojure/Clojure.swift
+++ Sources/Clojure/Clojure.swift
@@ -2,4 +2,16 @@ struct Clojure {
     func Take() {
         print("Take!")
     }
+    
+    func Yeah() {
+        Take()
+    }
+    
+    var a = 1
+    var isGood = false
 }
+
+func FN() {
+    print("MyFN!")
+}
+
--- Tests/ClojureTests/ClojureTests.swift
+++ Tests/ClojureTests/ClojureTests.swift
@@ -6,7 +6,9 @@ final class ClojureTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Clojure().text, "Hello, World!")
+        // XCTAssertEqual(Clojure().text, "Hello, World!")
+        
+        Clojure().Take()
     }
 
     static var allTests = [

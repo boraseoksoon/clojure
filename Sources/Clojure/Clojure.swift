8a508710953fd7ea3e7ceafe6b12fa444ba0c429
seoksoon <boraseoksoon@gmail.com>
2021-01-26 22:51:22 -0500

    [ADD] seriously basic wrappers

--- Sources/Clojure/Clojure.swift
+++ Sources/Clojure/Clojure.swift
@@ -1,17 +1,49 @@
-struct Clojure {
-    public func Take() {
-        print("Take!")
-    }
-    
-    public func Yeah() {
-        Take()
-    }
-    
-    public var a = 1
-    public var isGood = false
+//
+//  Clojure.swift
+//  Swift functions that implements Clojure standard library
+//
+//  Created by Seoksoon Jang on 2020/01/05.
+//  Copyright © 2021 LambdaDog. All rights reserved.
+//
+
+import Foundation
+
+public func Map<T>(_ seq: [T], closure: (T) -> Any) -> [Any] {
+    seq.map(closure)
 }
 
-public func FN() {
-    print("MyFN!")
+public func Filter<T>(_ seq: [T], closure: (T) throws -> Bool) -> [Any] {
+    try! seq.filter(closure)
 }
 
+public func Reduce<T>(from initialValue: T,
+                      sequence: [T],
+                      closure: (T, T) throws -> T) -> T {
+    try! sequence.reduce(initialValue, closure)
+}
+
+public func Take<T>(_ count: Int,
+                    _ sequence: Array<T>) -> Array<T>.SubSequence where T: Any {
+    sequence[0..<count]
+}
+
+// Clojure => (map (fn [i] (str i "!\n")) (range 0 10))
+//let res = Map(Array((0...10))) { String($0) + "\n!" }
+//res
+
+// Clojure => (filter (fn [item] (= (mod item 2) 0)) (range 0 10))
+// => (0 2 4 6 8 10)
+//let res2 = Filter(Array((0...10))) { $0.isMultiple(of: 2) }
+//res2
+// => [0, 2, 4, 6, 8, 10]
+
+// Clojure => (reduce + (range 0 10))
+// => 45
+//let res3 = Reduce(from: 0, sequence: Array((0...10))) { $0 + $1 }
+// => 55
+
+// Clojure => (take 3 (range 0 10))
+// (0 1 2)
+//let res4 = Take(3, Array(0...10))
+//res4
+// => [0,1,2]
--- Tests/ClojureTests/ClojureTests.swift
+++ Tests/ClojureTests/ClojureTests.swift
@@ -8,7 +8,28 @@ final class ClojureTests: XCTestCase {
         // results.
         // XCTAssertEqual(Clojure().text, "Hello, World!")
         
-        Clojure().Take()
+        // Clojure => (map (fn [i] (str i "!\n")) (range 0 10))
+        let res = Map(Array((0...10))) { String($0) + "\n!" }
+        res
+
+        // Clojure => (filter (fn [item] (= (mod item 2) 0)) (range 0 10))
+        // => (0 2 4 6 8 10)
+        let res2 = Filter(Array((0...10))) { $0.isMultiple(of: 2) }
+        res2
+        // => [0, 2, 4, 6, 8, 10]
+
+        // Clojure => (reduce + (range 0 10))
+        // => 45
+        let res3 = Reduce(from: 0, sequence: Array((0...10))) { $0 + $1 }
+        // => 55
+
+        // Clojure => (take 3 (range 0 10))
+        // (0 1 2)
+        let res4 = Take(3, Array(0...10))
+        res4
+        // => [0,1,2]
+
+        
     }
 
     static var allTests = [
